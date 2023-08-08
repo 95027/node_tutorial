277990db4227aca1bd8588e5dcd6ab155e965df0
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-08-08 20:40:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+
+/* const os = require("os");
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir()); */
+
+/* console.log(__dirname);
+console.log(__filename); */
+
+/* const path = require('path');
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename)); */
+
+/* const {add, div, sub,mul} = require("./math");
+console.log(div(2,3));
+console.log(add(2,3));
+console.log(mul(2,3));
+console.log(sub(2,3)); */
