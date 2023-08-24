d340e665c2c93adcbbcdeba274d9c262e079cc36
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-08-24 15:55:09 +0530

    "express routings"

---
+++ express/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get("^/$|index(.html)?", (req,res)=>{
+    //res.sendFile("./views/index.html", {root : __dirname});
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// for matching the routes / or index or index.html
+app.get("/new(.html)?", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "new.html"))
+});
+
+app.get("/old(.html)?", (req, res) => {
+    // 301 indicates redirecting permently
+    res.redirect(301,"/new.html") // 302 by default
+})
+
+
+//route handlers
+
+app.get("/hello(.html)?", (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next()
+}, (req, res) => {
+    res.send("hello world")
+})
+
+app.get("/*", (req, res) => {
+    //status for sending status code 
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>console.log("server is running on " + PORT));
